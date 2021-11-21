fb9359a354e15790bfd7fc13e4d74a8ffc90c951
K-MHoon <azmria117@gmail.com>
2021-11-21 16:30:27 +0900

    feat : Custom Repository Create Test

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -353,4 +353,9 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findLockByUsername("member1");
 
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
